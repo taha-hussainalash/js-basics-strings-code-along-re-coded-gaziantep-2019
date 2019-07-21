be9798c82924039788d54a61c1bf267c389fd5ce
Taha HussainAlash <taha.benzma94@gmail.com>
2019-07-21 10:26:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-/*const currentUser = 'Grace Hopper';
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;*/
-=======
 const currentUser = 'Grace Hopper';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
@@ -14,4 +6,3 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
->>>>>>> 12f381a5225d9d64700c05a0cb637f72f0807557
