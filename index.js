cd11a5c51f749749d8bfff4dd108e67e736b545d
Taha HussainAlash <taha.benzma94@gmail.com>
2019-07-21 02:58:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const currentUser = 'Grace Hopper';
+/*const currentUser = 'Grace Hopper';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;*/
